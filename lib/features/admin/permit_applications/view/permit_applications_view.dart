a80665ccc4c38697df16666cc84721360035cf9c
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-04-09 01:25:38 +0300

    WIP

--- lib/features/admin/permit_applications/view/permit_applications_view.dart
+++ lib/features/admin/permit_applications/view/permit_applications_view.dart
@@ -1,3 +1,6 @@
+import 'package:ecampusguard/global/widgets/app_bar.dart';
+import 'package:ecampusguard/global/widgets/app_logo.dart';
+import 'package:ecampusguard/global/widgets/responsive.dart';
 import 'package:ecampusguardapi/ecampusguardapi.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +19,47 @@ class PermitApplicationsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cubit = context.read<PermitApplicationsCubit>();
-    return Container(
-      child: Text(status.toString()),
+    var theme = Theme.of(context);
+    return Scaffold(
+      appBar: appBar,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            left: -150,
+            bottom: -150,
+            child: Opacity(
+              opacity: 0.2,
+              child: AppLogo(
+                darkMode: theme.colorScheme.brightness == Brightness.dark,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: ResponsiveWidget.defaultPadding(context),
+              vertical: ResponsiveWidget.mediumPadding(context),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Applications",
+                      style: theme.textTheme.headlineSmall,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.filter_alt),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
